49c3242bbc17d210a1891e24ae64d462c3161bd4
kakavidon <kakavidon@gmail.com>
2016-05-28 12:26:35 +0300

    TEST

--- src/net/adminbg/merger/AdminMerger.java
+++ src/net/adminbg/merger/AdminMerger.java
@@ -3,16 +3,19 @@ package net.adminbg.merger;
 import net.adminbg.merger.ui.MainWindow;
 
 /**
- *
+ * 
  * @author lachezar.nedelchev
  */
 public class AdminMerger {
+	private static final Logger logger = Logger.getLogger(AdminMerger.class
+			.getName());
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        MainWindow.start();
-    }
-    
+	/**
+	 * @param args
+	 *            the command line arguments
+	 */
+	public static void main(String[] args) {
+		MainWindow.start();
+	}
+
 }
